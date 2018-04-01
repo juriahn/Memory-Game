e9493d1868270e032a231eafbbe4e92406af4fbf
Juri <juriahn817@gmail.com>
2018-03-31 19:06:39 -0500

    Fixed minor bugs and incremented moves counter & added refresh fucntion
    Fixed a bug where the card can match itself, added the refresh function where it would reset the game, and added the increment function where it displays total moves

--- js/app.js
+++ js/app.js
@@ -21,10 +21,12 @@ let allCards = [
   "fa-bicycle"
 ];
 
-let cardsClicked = 0;
+let clickCounter = 0;
 let storeCardClassOne = "";
 let storeCardClassTwo = "";
-
+let allowNextTurn = true;
+let numberOfTotalMoves = 0;
+let numberOfPairsMatched = 0;
 /*
  * Display the cards on the page
  *   - shuffle the list of cards using the provided "shuffle" method below
@@ -33,6 +35,20 @@ let storeCardClassTwo = "";
  */
 
 // Shuffle function from http://stackoverflow.com/a/2450976
+function startGame() {
+    clickCounter=0;
+    storeCardClassOne="";
+    storeCardClassTwo="";
+    allowNextTurn = true;
+    numberOfPairsMatched = 0;
+    numberOfTotalMoves = 0;
+    document.getElementById("moves").innerHTML = "0";
+    $("#deck").empty();
+    addCardsToDeck();
+}
+
+startGame();
+
 function shuffle(array) {
   var currentIndex = array.length,
     temporaryValue,
@@ -49,16 +65,18 @@ function shuffle(array) {
   return array;
 }
 
-const shuffledCards = shuffle(allCards);
+function addCardsToDeck() {
+  const shuffledCards = shuffle(allCards);
 
-for (cards in shuffledCards) {
-  const deckReference = $("#deck");
-  const listItem = `<li class= 'card' id="card"><i id="shuffledCard" class ='fa ${
-    shuffledCards[cards]
-  }'></i></li>`;
-  const listItemHTML = createElementFromHTML(listItem);
-  listItemHTML.addEventListener("click", onCardClick);
-  deckReference.append(listItemHTML);
+  for (cards in shuffledCards) {
+    const deckReference = $("#deck");
+    const listItem = `<li class= 'card' id="card"><i id="shuffledCard" class ='fa ${
+      shuffledCards[cards]
+    }'></i></li>`;
+    const listItemHTML = createElementFromHTML(listItem);
+    listItemHTML.addEventListener("click", onCardClick);
+    deckReference.append(listItemHTML);
+  }
 }
 
 function createElementFromHTML(htmlString) {
@@ -69,49 +87,133 @@ function createElementFromHTML(htmlString) {
   return div.firstChild;
 }
 
-// const clickCard = $('.card').click((appendShowClass()));
-
 function onCardClick() {
-    if (cardsClicked > 2) {
-        return;
+  if (!allowNextTurn) {
+    return;
+  }
+  revealCard(this);
+  incrementClickCounter(this);
+  storeListItem(this);
+  checkIfCardsMatch();
+  incrementTotalAttempts();
+  resetCounterAfterSecondCLick();
+  endGame();
+  console.log(this);
+}
+
+function revealCard(listItem) {
+  listItem.classList.add("show", "open");
+}
+
+function incrementClickCounter(listItem) {
+  if (clickCounter === 1) {
+    if (!(listItem === storeCardClassOne)) {
+      clickCounter++;
     }
-  this.classList.add("show", "open");
-  cardsClicked++;
-  matchCards(this);
-}
-
-function matchCards(listItem) {
-  //storing each card clicked into a variable
-  if (cardsClicked === 1) {
-    storeCardClassOne = listItem;
-    console.log(storeCardClassOne);
-  } else if (cardsClicked === 2) {
-    storeCardClassTwo = listItem;
-    console.log(storeCardClassTwo);
-    //check to see if the cards match
-    checkSecondCondition();
-  }
-}
-
-const removeOpenAndShowClass = () => {
-  setTimeout(function() {
-    storeCardClassOne.classList.remove("open", "show");
-    storeCardClassTwo.classList.remove("open", "show");
-  }, 500);
-};
-
-function checkSecondCondition() {
-  if (storeCardClassOne.innerHTML === storeCardClassTwo.innerHTML) {
-    storeCardClassOne.classList.add("match");
-    storeCardClassTwo.classList.add("match");
-    cardsClicked = 0;
-    console.log("its a match!");
   } else {
-    removeOpenAndShowClass();
-    cardsClicked = 0;
-    console.log("nope! Try again!");
+    clickCounter++;
   }
 }
+
+function storeListItem(itemClicked) {
+  if (clickCounter === 1) {
+    storeCardClassOne = itemClicked;
+  } else if (clickCounter === 2) {
+    storeCardClassTwo = itemClicked;
+  }
+}
+
+function resetCounterAfterSecondCLick() {
+  if (clickCounter === 2) {
+    clickCounter = 0;
+  }
+}
+
+function checkIfCardsMatch() {
+  if (clickCounter === 2) {
+    if (ifCardsMatch()) {
+      addMatchClassToCards();
+      numberOfPairsMatched++;
+      console.log("number of matched pairs:" + numberOfPairsMatched);
+    } else {
+      removingNonMatchingCardClasses();
+    }
+  }
+}
+
+function incrementTotalAttempts(){
+    if(clickCounter===2){
+        numberOfTotalMoves++;
+        document.getElementById("moves").innerHTML = numberOfTotalMoves;
+
+        console.log("total moves: " ,numberOfTotalMoves);
+    }
+}
+
+function addMatchClassToCards() {
+  storeCardClassOne.classList.add("match");
+  storeCardClassTwo.classList.add("match");
+}
+
+function ifCardsMatch() {
+  return storeCardClassOne.innerHTML === storeCardClassTwo.innerHTML;
+}
+
+function removingNonMatchingCardClasses() {
+  allowNextTurn = false;
+  setTimeout(() => {
+    storeCardClassOne.classList.remove("show", "open");
+    storeCardClassTwo.classList.remove("show", "open");
+    allowNextTurn = true;
+  }, 600);
+}
+
+function endGame() {
+  if (numberOfPairsMatched === 8) {
+    setTimeout(() => {
+      alert("you have finished the game!");
+    }, 300);
+  }
+}
+
+// function onCardClick() {
+//   this.classList.add("show", "open");
+//   cardsClicked++;
+//   matchCards(this);
+// }
+
+// function matchCards(listItem) {
+//   //storing each card clicked into a variable
+//   if (cardsClicked === 1) {
+//     storeCardClassOne = listItem;
+//     console.log(storeCardClassOne);
+//   } else if (cardsClicked === 2) {
+//     storeCardClassTwo = listItem;
+//     console.log(storeCardClassTwo);
+//     //check to see if the cards match
+//     checkSecondCondition();
+//   }
+// }
+
+// const removeOpenAndShowClass = () => {
+//   setTimeout(function() {
+//     storeCardClassOne.classList.remove("open", "show");
+//     storeCardClassTwo.classList.remove("open", "show");
+//   }, 500);
+// };
+
+// function checkSecondCondition() {
+//   if (storeCardClassOne.innerHTML === storeCardClassTwo.innerHTML) {
+//     storeCardClassOne.classList.add("match");
+//     storeCardClassTwo.classList.add("match");
+//     cardsClicked = 0;
+//     console.log("its a match!");
+//   } else {
+//     removeOpenAndShowClass();
+//     cardsClicked = 0;
+//     console.log("nope! Try again!");
+//   }
+// }
 /*
  * set up the event listener for a card. If a card is clicked:
  *  - display the card's symbol (put this functionality in another function that you call from this one)
